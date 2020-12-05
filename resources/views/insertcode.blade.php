e725b1da1c3aa49143033a5963f5e61aed7db701
43461293 <galprad99@gmail.com>
2020-12-05 11:20:18 +0700

    add barrier pemilih

--- resources/views/insertcode.blade.php
+++ resources/views/insertcode.blade.php
@@ -10,6 +10,10 @@
                 <div class="my-4">
                   <p class="lead">Isi kode rahasia yang dikirim ke email Student Unpad untuk menggunakan hak pilih.</p>
                   <p><small>Kami menggunakan teknologi Cookies & Javascript, pastikan browser kamu mengaktifkan fitur Javascript dan menerima Cookies.</small></p>
+                 
+                  @if(\Carbon\Carbon::now()->timezone('Asia/Jakarta')->format('Y-m-d') >= $tanggalpemilihan->setting_value && \Carbon\Carbon::now()->timezone('Asia/Jakarta')->format('Y-m-d') <= \Carbon\Carbon::parse($tanggalpemilihan->setting_value)->addDays(4))
+               @if( strtotime(\Carbon\Carbon::now()->timezone('Asia/Jakarta')->format('H:i')) >= strtotime($jammulaipemilihan->setting_value) && strtotime(\Carbon\Carbon::now()->timezone('Asia/Jakarta')->format('H:i')) <= strtotime($jamakhirpemilihan->setting_value) ) 
+                 
                   <form method="POST" action="{{url('pilih')}}">
     @csrf 
     <div class="form-group row mt-4">
@@ -24,6 +28,16 @@
 </div> 
 
   </form>
+  @else
+  Belum Jam Pemilihan
+  @endif
+  
+  @else
+  <div class="alert alert-warning" role="alert">
+Belum Hari Pemilihan <br>
+Hari Pemilihan adalah {{date('d M Y', strtotime($tanggalpemilihan->setting_value))}} dimulai pukul {{$jammulaipemilihan->setting_value}} - {{$jamakhirpemilihan->setting_value}} WIB
+</div>
+  @endif
   <div id="msgs"></div>
               
                 </div>
